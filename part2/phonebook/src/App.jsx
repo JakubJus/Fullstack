afab9f4bb7e19524ee8ea705f165bc2a72138594
Jakub Juszkiewicz <150861754+JakubJus@users.noreply.github.com>
2024-01-09 20:28:48 +0100

    Update App.jsx

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,47 +1,75 @@
-import { useState } from 'react'
-import Note from './components/Note'
+import React, { useState } from 'react';
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes) 
-  const [newNote, setNewNote] = useState('')
-  const [showAll, setShowAll] = useState(true)
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ]);
+  const [newName, setNewName] = useState('');
+  const [newNum, setNewNum] = useState('');
+  const [look, setLook] = useState('');
 
-  const addNote = (event) => {
-    event.preventDefault()
-    const noteObject = {
-      content: newNote,
-      important: Math.random() < 0.5,
-      id: notes.length + 1,
+  const handleNumChange = (event) => {
+    console.log(event.target.value);
+    setNewNum(event.target.value);
+  };
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+
+  const checkExist = () => persons.some(contact => contact.name.toLowerCase() === newName.toLowerCase());
+
+  const handleAddPerson = (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+
+    if (!checkExist()) {
+      setPersons([...persons, { name: newName, number: newNum }]);
+      setNewName(''); // Clear the input fields after adding a person
+      setNewNum('');
+    } else {
+      alert(`${newName} is already added to the phonebook`);
     }
-  
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
-  }
-  const handleNoteChange = (event) => {
-    console.log(event.target.value)
-    setNewNote(event.target.value)
-  }
-  const notesToShow = showAll
-  ? notes
-  : notes.filter(note => note.important === true)
+  };
 
-return (
-  <div>
-    <h1>Notes</h1>
+  const handleSearth = (event) => {
+    console.log(event.target.value);
+    setLook(event.target.value);
+  };
+
+  const filteredPersons = persons.filter(contact =>
+    contact.name.toLowerCase().includes(look.toLowerCase())
+  );
+
+  return (
     <div>
-        <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all' }
-        </button>
+      <h1>Phonebook</h1>
+      <div>
+        search: <input value={look} onChange={handleSearth} />
       </div>
-      <form onSubmit={addNote}>
-        <input
-          value={newNote}
-          onChange={handleNoteChange}
-        />
-        <button type="submit">save</button>
-      </form>   
+      <h2>Add a new</h2>
+      <form onSubmit={handleAddPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNum} onChange={handleNumChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {filteredPersons.map((person, index) => (
+          <li key={index}>{person.name} - {person.number}</li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App 
+export default App;
