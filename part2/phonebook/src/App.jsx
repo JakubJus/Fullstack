3b60376476e20fd99dd70c66a276f15a4c12db03
Jakub Juszkiewicz <150861754+JakubJus@users.noreply.github.com>
2024-01-15 21:41:38 +0100

    Update App.jsx

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,174 +1,140 @@
 import React, { useState, useEffect } from 'react';
-import Note from './components/Notes';
+import Display from './components/People';
 import ServerCom from './services/Persons';
 import "./index.css";
 
-
-const Notification = ({ message, type }) => {
-  if (message === null) {
-    return null;
-  }
-  console.log("type: ",type)
-
-  return (
-    <div className={type}>
-      {message}
-    </div>
-  );
-};
-
-const TextBox = (props) => (
-  <div>
-    <input placeholder={props.text} onChange={props.onChange} />
-  </div>
-);
-
-const RemoveButton = (props) => {
-  return (
-    <button onClick={() => props.removePerson(props.id)}>
-      {props.text}
-    </button>
-  );
-};
-
-const Button = (props) => (
-    <button onClick={props.handleClick}>
-      {props.text}
-    </button>
-);
-
 const App = () => {
-  const [persons, setPersons] = useState([]);
-  const [newName, setNewName] = useState('');
-  const [newNum, setNewNum] = useState('');
-  const [look, setLook] = useState('');
-  const [errorMessage, setErrorMessage] = useState(null);
-  const[errorState, setErrorState] = useState('');
-
+  const [persons, setPersons] = useState([]); // name and number list
+  const [newName, setNewName] = useState(''); //gets name
+  const [newNum, setNewNum] = useState(''); //gets number
+  const [look, setLook] = useState(''); //filter on name
+  const [alertState, setAlertState] = useState(''); //determine if positive of negative statement
+  const [alertText, setAlertText] = useState(null); //text of the error
+  
   const checkPersonExist = () => persons.some(contact => contact.name.toLowerCase() === newName.toLowerCase());
   const checkNumExist = () => persons.some(contact => contact.number.toLowerCase() === newNum.toLowerCase());
 
   useEffect(() => {
-    ServerCom
-      .getAll()
+    ServerCom.getAll()
       .then(existingContacts => {
         setPersons(existingContacts)
       })
   }, [])
+
+  const clearAlert = () => {
+    setAlertText(null);
+  };
 
   const removePerson = id => {
     if (window.confirm('Are you sure you want to remove?')) {
       ServerCom.remove(id)
         .then(() => {
           setPersons(prevPersons => prevPersons.filter(person => person.id !== id));
+        
+          setAlertText(`Successfully removed ${newName} with number ${newNum} from server`);
+          setAlertState('sucess')
+          setTimeout(clearAlert, 5000);
+
         })
         .catch(error => {
-          console.error('Error removing person from server:', error);
+          //console.error('Error removing person from server:', error);
+          setAlertText(`Error removing ${newName} with number ${newNum} from server`);
+          setAlertState('error')
+          setTimeout(clearAlert, 5000);
         })
     }
   }
   
   const changeVal = (event,id) => {
     event.preventDefault();
-   
     const noteObject = {
         id: id,
         name: newName,
         number: newNum
-    };
-
-    setErrorMessage(
-      `Sucess:changed value to ${newName} with number ${newNum}`
-  );
-  setErrorState('sucess')
-  setTimeout(() => {
-      setErrorMessage(null);
-  }, 5000);
+    }
 
     ServerCom.changeValue(noteObject)
         .then(response => {
-            console.log('Server response:', response);
-            // Verify the structure of the response and adjust this line accordingly
-            ServerCom
-              .getAll()
+            //console.log('Server response:', response);
+            ServerCom.getAll()
               .then(existingContacts => {
-              setPersons(existingContacts)
+                setPersons(existingContacts)
+
+                setAlertText(`Sucess: Added new contact`);
+                setAlertState('sucess')
+                setTimeout(clearAlert, 5000);
             })
             setNewName('');
             setNewNum('');
         })
         .catch(error => {
-            console.error('Error adding person to server:', error);
+          setAlertText(`Error: Adding new contact`);
+          setAlertState('error')
+          setTimeout(clearAlert, 5000);
+          //console.error('Error adding person to server:', error);
         });
-};
+  }
+
   const addToServer = event => {
     event.preventDefault();
-  
+
     const noteObject = {
       name: newName,
       number: newNum,
     };
-    setErrorMessage(
-      `Adding ${newName} with number ${newNum} to server`
-  );
-  setErrorState('sucess')
-  setTimeout(() => {
-      setErrorMessage(null);
-  }, 5000);
-    console.log('Adding to server:', noteObject);
+    //console.log('Adding to server:', noteObject);
   
     ServerCom.create(noteObject)
       .then(response => {
-        console.log('Server response:', response);
-        console.log('Server responseid:', persons);
+        //console.log('Server response:', response);
+        //console.log('Server responseid:', persons);
         // Verify the structure of the response and adjust this line accordingly
         setPersons([...persons, response]);
-  
         setNewName('');
         setNewNum('');
+
+        setAlertText(`Success: Adding ${newName} with number ${newNum} to server`);
+        setAlertState('sucess')
+        setTimeout(clearAlert, 5000);
       })
       .catch(error => {
-        console.error('Error adding person to server:', error);
+        setAlertText(`Error: Adding ${newName} with number ${newNum} to server`);
+        setAlertState('error')
+        setTimeout(clearAlert, 5000);
+        //console.error('Error adding person to server:', error);
       });
-  };
+  }
   
   const handleAddPerson = (event) => {
     event.preventDefault();
+
     if ( newName==="") {
-      setErrorMessage(
-        `Please enter a name`
-      );
-      setErrorState('error');
-      console.log("Errorstate: ",errorState);
-    setTimeout(() => {
-        setErrorMessage(null);
-    }, 5000);
+      setAlertText(`Please enter a name`);
+      setAlertState('error');
+      setTimeout(clearAlert, 5000);
+      //console.log("alertState: ",alertState);
       return
     }
+
     if( newNum===""){
-      setErrorMessage(
-        `Please enter a number`
-    );
-    setErrorState('error')
-    setTimeout(() => {
-        setErrorMessage(null);
-    }, 5000);
+      setAlertText(`Please enter a number`);
+      setAlertState('error')
+      setTimeout(clearAlert, 5000);
       return
     }
-    if (!checkPersonExist() || !checkNumExist()) {
+    if (!checkPersonExist() || !checkNumExist()) { //IF ONE OF THEM DOSENT EXIST->CHANGE NUM OR CHANGE NAME OR ADD
       if(checkPersonExist()){
         const noteId = persons.find(n => n.name.toLowerCase() === newName.toLowerCase());
         if(confirm(`Name already exist\nDo you want to change the number for ${noteId.name}\nFrom: ${noteId.number}\nTo: ${newNum}`)){
-          console.log("NoteID is:", noteId);
+          //console.log("NoteID is:", noteId);
           changeVal(event, noteId.id);
           return;
         }
       }
-
       if(checkNumExist()){
         const noteId = persons.find(n => n.number.toLowerCase() === newNum.toLowerCase());
         if(confirm(`Number already exist\nDo you want to change the name for number: ${noteId.number}\nFrom: ${noteId.name}\nTo: ${newName}`)){
-          console.log("NoteID is:", noteId);
+          //console.log("NoteID is:", noteId);
           changeVal(event, noteId.id);
           return;
         }
@@ -176,55 +142,50 @@ const App = () => {
 
       if (!checkPersonExist() && !checkNumExist()) {
         addToServer(event);
-        
       }
     } else {
-      setErrorMessage(
-        `Name ${newName} with number ${newNum} was already added to server`
-    );
-    setErrorState('error')
-    setTimeout(() => {
-        setErrorMessage(null);
-    }, 5000);
+      setAlertText(`Name ${newName} with number ${newNum} was already added to server`);
+      setAlertState('error')
+      setTimeout(clearAlert, 5000);
     }
-  };
+  }
 
   const handleNumChange = (event) => {
-    console.log(event.target.value);
+    //console.log(event.target.value);
     setNewNum(event.target.value);
-  };
+  }
 
   const handleNameChange = (event) => {
-    console.log(event.target.value);
+    //console.log(event.target.value);
     setNewName(event.target.value);
-  };
+  }
 
   const handleSearth = (event) => {
-    console.log(event.target.value);
+    //console.log(event.target.value);
     setLook(event.target.value);
-  };
+  }
 
   const filteredPersons = persons.filter(contact =>
     contact.name.toLowerCase().includes(look.toLowerCase())
-  );
+  );//SEARTH ON NAME
 
   return (
     <div>
       <h1>Phonebook</h1>
-      <Notification message={errorMessage} type={errorState} />
-      <TextBox onChange={handleSearth} text="Searth" />
+      <Display.Notification message={alertText} type={alertState} />
+      <Display.TextBox onChange={handleSearth} text="Searth" />
       <h2>Add a new</h2>
       <form onSubmit={handleAddPerson}>
-        <TextBox onChange={handleNameChange} text="Name" />
-        <TextBox onChange={handleNumChange} text="Number" />
-        <Button type="submit" text="Submit" />
+        <Display.TextBox onChange={handleNameChange} text="Name" />
+        <Display.TextBox onChange={handleNumChange} text="Number" />
+        <Display.Button type="submit" text="Submit" />
       </form>
       <h2>Numbers</h2>
       <ul>
         {filteredPersons.map((person, index) => (
         <li key={index}>
         {person.name} - {person.number}
-        <RemoveButton removePerson={removePerson} id={person.id} text="Remove" />
+        <Display.RemoveButton removePerson={removePerson} id={person.id} text="Remove" />
         </li>
         ))}
       </ul>
