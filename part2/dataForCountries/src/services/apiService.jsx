bf69d5bbd97e6c2462357c4e842fccbe7d083567
Jakub Juszkiewicz <150861754+JakubJus@users.noreply.github.com>
2024-01-16 15:28:37 +0100

    Update apiService.jsx

--- part2/dataForCountries/src/services/apiService.jsx
+++ part2/dataForCountries/src/services/apiService.jsx
@@ -1,21 +1,25 @@
 import axios from 'axios';
 
-const apiService = {
-  searchCountries: async (value) => {
-    try {
-      const response = await axios.get(`https://restcountries.com/v3.1/name/${value}`);
-      return response.data.map((countryData) => ({
-        name: countryData.name.common,
-        capital: countryData.capital,
-        area: countryData.area,
-        languages: countryData.languages,
-        flags: countryData.flags,
-      }));
-    } catch (error) {
-      console.error('Error fetching country data:', error);
-      return [];
-    }
-  },
-};
 
-export default apiService;
+
+const apiService = {
+    searchCountries: async (value) => {
+      try {
+        const response = await axios.get(`https://restcountries.com/v3.1/name/${value}`);
+        return response.data.map((countryData) => ({
+          name: countryData.name.common,
+          capital: countryData.capital,
+          area: countryData.area,
+          languages: countryData.languages,
+          flags: countryData.flags,
+          capitalinfo: countryData.capitalinfo,
+          latLng: countryData.latlng,
+        }));
+      } catch (error) {
+        console.error('Error fetching country data:', error);
+        return [];
+      }
+    },
+  };
+  
+  export default apiService;
