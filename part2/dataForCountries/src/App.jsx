6f6aa6ed178520dae89e89eaac3634c6f91131b0
Jakub Juszkiewicz <150861754+JakubJus@users.noreply.github.com>
2024-01-16 15:27:58 +0100

    Update App.jsx

--- part2/dataForCountries/src/App.jsx
+++ part2/dataForCountries/src/App.jsx
@@ -1,24 +1,32 @@
 import React, { useState, useEffect } from 'react';
-import apiService from './services/apiService';
 import SearchForm from './components/SearchForm';
 import CountryDetails from './components/CountryDetails';
-
+import apiService from './services/apiService';
+import weatherApi from './services/weatherApi';
 
 const App = () => {
   const [value, setValue] = useState('');
   const [countryList, setCountryList] = useState([]);
   const [suggestions, setSuggestions] = useState([]);
+  const [weather, setWeather] = useState(null);
 
   useEffect(() => {
-    // Skip if value is not defined
     if (value) {
       apiService.searchCountries(value).then((countries) => {
         if (countries.length > 1) {
           setCountryList(countries);
           setSuggestions(countries.map((country) => country.name));
         } else if (countries.length === 1) {
-          setCountryList(countries);
+          setCountryList(countries.map(country => ({ ...country, weather: null })));
           setSuggestions([]);
+          const [latitude, longitude] = countries[0].latLng;
+          weatherApi.getWeather(latitude, longitude)
+            .then(weatherData => {
+              setCountryList([{
+                ...countries[0],
+                weather: weatherData
+              }]);
+            });
         } else {
           setCountryList([]);
           setSuggestions([]);
