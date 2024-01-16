30beaa42715aa96c0c28eacb5751ac6a147b22cd
Jakub Juszkiewicz <150861754+JakubJus@users.noreply.github.com>
2024-01-16 10:18:22 +0100

    Add files via upload

---
+++ part2/dataForCountries/src/App.jsx
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from 'react';
+import apiService from './services/apiService';
+import SearchForm from './components/SearchForm';
+import CountryDetails from './components/CountryDetails';
+
+
+const App = () => {
+  const [value, setValue] = useState('');
+  const [countryList, setCountryList] = useState([]);
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    // Skip if value is not defined
+    if (value) {
+      apiService.searchCountries(value).then((countries) => {
+        if (countries.length > 1) {
+          setCountryList(countries);
+          setSuggestions(countries.map((country) => country.name));
+        } else if (countries.length === 1) {
+          setCountryList(countries);
+          setSuggestions([]);
+        } else {
+          setCountryList([]);
+          setSuggestions([]);
+        }
+      });
+    }
+  }, [value]);
+
+  const handleChange = (event) => {
+    const inputValue = event.target.value;
+    setValue(inputValue);
+
+    if (inputValue && countryList.length === 5) {
+      setSuggestions(countryList.map((country) => country.name));
+    } else {
+      setSuggestions([]);
+    }
+  };
+
+  const onSearch = (event) => {
+    event.preventDefault();
+    setCountryList([]);
+    setSuggestions([]);
+    setValue(value.toLowerCase());
+  };
+
+  const handleSuggestionClick = (suggestion) => {
+    setValue(suggestion);
+    setCountryList([]);
+    setSuggestions([]);
+  };
+
+  return (
+    <div>
+      <SearchForm
+        value={value}
+        handleChange={handleChange}
+        onSearch={onSearch}
+        suggestions={suggestions}
+        handleSuggestionClick={handleSuggestionClick}
+      />
+      {countryList.length === 1 && <CountryDetails country={countryList[0]} />}
+    </div>
+  );
+};
+
+export default App;
