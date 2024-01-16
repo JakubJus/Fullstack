6efe1cc5b9a09f265b86d3621d00542b4dc25b5b
Jakub Juszkiewicz <150861754+JakubJus@users.noreply.github.com>
2024-01-16 15:30:26 +0100

    Update CoutryDetails.jsx

--- part2/dataForCountries/src/components/CoutryDetails.jsx
+++ part2/dataForCountries/src/components/CoutryDetails.jsx
@@ -5,15 +5,33 @@ const CountryDetails = ({ country }) => (
     <h1>{country.name}</h1>
     {country.capital && <p>Capital: {country.capital}</p>}
     {country.area && <p>Area: {country.area}</p>}
-    <br />
+    <br/>
     <h3>Languages</h3>
-    {country.languages && <p>{Object.values(country.languages).join(', ')}</p>}
+    {country.languages && (
+    <ul>
+    {Object.entries(country.languages).map(([code, language], index) => (
+      <li key={index}>{`${language}`}</li>
+    ))}
+    </ul>
+    )}
     {country.flags && (
       <img
         src={country.flags.svg}
         alt={`Flag of ${country.name}`}
         style={{ width: '100px', height: 'auto' }}
       />
+    )}
+     {country.weather && (
+      <div>
+        <h3>Weather in {country.capital}:</h3>
+        <p>Temperature: {country.weather.temp.toFixed(2)}°C</p>
+          <img
+            src={`https://openweathermap.org/img/wn/${country.weather.icon}@2x.png`}
+            alt={`Weather icon for ${country.name}`}
+            style={{ width: '50px', height: '50px' }}
+          />
+       <p>Windspeed: {country.weather.windspeed} m/s</p>
+      </div>
     )}
   </div>
 );
